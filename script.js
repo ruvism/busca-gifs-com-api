d7a8ab3a945fefec988c7afaf66a0cfa6e71586e
Rúvila Avelino <42655569+ruvism@users.noreply.github.com>
2018-10-19 11:01:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const btnBusca = document.getElementById("btn-busca");
+btnBusca.addEventListener("click", trazResultadoDaBusca);
+let listaGifs = [];
+
+function buscaGif(){
+  return document.getElementById("campo-busca").value;
+}
+
+function erro(){
+  console.log("erro");
+}
+
+function trazResultadoDaBusca(event){
+  event.preventDefault();
+  const respostaDaBusca = new XMLHttpRequest();
+  respostaDaBusca.open("GET",` http://api.giphy.com/v1/gifs/search?q=${buscaGif()}&api_key=az5qe2U1BYVwtvWgrDjVucEoknKtxWjW&`);
+  respostaDaBusca.onload = carregaPostsComGifs;
+  respostaDaBusca.onerror = erro;
+  respostaDaBusca.send();
+}
+
+function carregaPostsComGifs(){
+  listaGifs = JSON.parse(this.responseText)["data"];
+  exibePosts();
+}
+
+function exibePosts(){
+  let exibeBusca = document.getElementById("exibe-busca");
+    exibeBusca.innerHTML = 
+      `<div class="area-gif"> ${listaGifs.map(gif => `
+        <div class="gif">
+          <img src="${gif.images.fixed_height.url}"></img>
+        </div>
+        `).join("")}
+      </div>`;
+}
+  
+
+
